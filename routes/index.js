460e78f9a7bd3e6abd3621246122902c2d8881ef
sagormax <sagor.touch@gmail.com>
2019-08-28 23:54:36 +0600

    Added admin router & hook it to index router js

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,11 @@ const app     = require('express');
 const router  = app.Router();
 
 const Home    = require('../app/controllers/HomeController');
+const Admin   = require('./admin');
 
 router.get('/', Home.index);
+
+// # Add admin route
+router.use('/admin', Admin);
 
 module.exports = router;
